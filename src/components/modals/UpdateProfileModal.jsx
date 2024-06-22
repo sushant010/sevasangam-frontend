9ce3d097fee167c02d8bc8001bc9adf2d8a0678b
ashi <ashi888032@gmail.com>
2024-06-22 23:50:32 +0530

    changes in the updateprofile

--- src/components/modals/UpdateProfileModal.jsx
+++ src/components/modals/UpdateProfileModal.jsx
@@ -5,6 +5,7 @@ import { toast } from 'react-toastify';
 import { useAuth } from '../../context/Auth';
 import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input';
 import compress from 'compress-base64'
+import LoadingSpinner from '../loadingSpinner/LoadingSpinner';
 
 const UpdateProfileModal = () => {
   const api = import.meta.env.VITE_API_URL;
@@ -72,13 +73,29 @@ const UpdateProfileModal = () => {
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
       fileReader.onload = event => {
-        compress(event.target.result, {
-          width: 400,
-          type: 'image/jpg',
-          max: 200, // max size
-          min: 20, // min size
-          quality: 0.8,
-        }).then(result => {
+        compress(event.target.result,
+          {
+            width: 400, // Increased width for better details
+            type: 'image/jpeg',
+            max: 50, // Increased max size to allow for higher quality
+            min: 10,  // Decreased min size to allow for smaller file sizes
+            quality: 0.95, // Adjusted quality to improve image fidelity
+          },
+          {
+            width: 400, // Increased width for better details
+            type: 'image/png',
+            max: 50, // Increased max size to allow for higher quality
+            min: 10,  // Decreased min size to allow for smaller file sizes
+            quality: 0.95, // Adjusted quality to improve image fidelity
+          },
+          {
+            width: 400, // Increased width for better details
+            type: 'image/webp',
+            max: 50, // Increased max size to allow for higher quality
+            min: 10,  // Decreased min size to allow for smaller file sizes
+            quality: 0.95, // Adjusted quality to improve image fidelity
+          },
+        ).then(result => {
           console.log(result)
           setAvatar(result)
         })
@@ -111,62 +128,65 @@ const UpdateProfileModal = () => {
   }, [auth]);
 
   return (
-    <div className="modal fade" id="updateProfileModal" tabIndex={-1} aria-labelledby="updateProfileModalLabel" aria-hidden="true">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5" id="updateProfileModalLabel">Update Profile</h1>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-          </div>
-          <div className="modal-body">
-            <div className="mb-3">
-              <input placeholder="Name" type="text" name="name" onChange={handleChange} value={credentials.name} className="form-control" id="name" />
+    <>
+      <div className="modal fade" id="updateProfileModal" tabIndex={-1} aria-labelledby="updateProfileModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="updateProfileModalLabel">Update Profile</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
             </div>
-            <div className="mb-3">
-              <PhoneInput placeholder='Mobile' value={credentials.phone} onChange={phoneNumChange} defaultCountry="IN" className="form-control input-form-control" international />
-            </div>
-            {/* <div className="mb-3">
+            <div className="modal-body">
+              <div className="mb-3">
+                <input placeholder="Name" type="text" name="name" onChange={handleChange} value={credentials.name} className="form-control" id="name" />
+              </div>
+              <div className="mb-3">
+                <PhoneInput placeholder='Mobile' value={credentials.phone} onChange={phoneNumChange} defaultCountry="IN" className="form-control input-form-control" international />
+              </div>
+              {/* <div className="mb-3">
               <input placeholder="Password" type="password" name="password" onChange={handleChange} value={credentials.password} className="form-control" id="password" />
             </div> */}
-            <div className="mb-3">
-              <a
-                style={{
-                  cursor: "pointer",
-                  textDecoration: "underline",
-                  color: "var(--color-theme-primary)",
-                }}
-                data-bs-toggle="modal"
-                data-bs-target="#forgotPasswordBackdrop"
-              >
-                Change Password
-              </a>
-            </div>
-            <label htmlFor="avatar" className="form-label">Avatar Image</label>
-            {avatarLoading == true ? <div className="spinner-border text-primary" role="status" /> : <label htmlFor="avatar" className='d-block custom-file-upload mb-2'>
-              <div style={{ width: "60px", height: "60px", borderRadius: "50px", overflow: "hidden" }}>
-                <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src={avatar || auth?.user?.avatar} alt="" />
+              <div className="mb-3">
+                <a
+                  style={{
+                    cursor: "pointer",
+                    textDecoration: "underline",
+                    color: "var(--color-theme-primary)",
+                  }}
+                  data-bs-toggle="modal"
+                  data-bs-target="#forgotPasswordBackdrop"
+                >
+                  Change Password
+                </a>
               </div>
-            </label>}
+              <label htmlFor="avatar" className="form-label">Avatar Image</label>
+              <label htmlFor="avatar" className='d-block custom-file-upload mb-2'>
+                <div style={{ width: "60px", height: "60px", borderRadius: "50px", overflow: "hidden" }}>
+                  <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src={avatar || auth?.user?.avatar} alt="" />
+                </div>
+              </label>
 
-            <input
-              type="file"
-              label="avatar"
-              name="avatar"
-              id='avatar'
-              className="form-control"
-              accept='.jpeg, .png, .jpg'
-              onChange={(e) => handleFileUpload(e)}
-            />
+              <input
+                type="file"
+                label="avatar"
+                name="avatar"
+                id='avatar'
+                className="form-control"
+                accept='.jpeg, .png, .jpg'
+                onChange={(e) => handleFileUpload(e)}
+              />
 
 
 
-          </div>
-          <div className="modal-footer flex-column">
-            <button type="button" onClick={handleSubmit} className="m-auto btn btn-theme-primary">Update</button>
+            </div>
+            <div className="modal-footer flex-column">
+              <button type="button" onClick={handleSubmit} className="m-auto btn btn-theme-primary">Update</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {avatarLoading && <LoadingSpinner text="Have Patience, Loading your image." />}
+    </>
   );
 };
 
